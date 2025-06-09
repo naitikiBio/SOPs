2d540b293eb61032bb4816e6bd8b0a61ee8d5ab6
naitikiBio <naitikgcpcertifications@gmail.com>
2025-06-09 13:38:30 -0400

    Update etl_workflow_template.py

--- etl_workflow_template.py
+++ etl_workflow_template.py
@@ -8,3 +8,8 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.email import send_email
+
+# --- Configuration Section ---
+# Define configurable parameters for your ETL DAG.
+# These can be accessed within tasks using config["key_name"].
+# For dynamic values like execution date, Airflow Jinja templating is used.
