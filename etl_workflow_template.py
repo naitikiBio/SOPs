2c99fabdf8fe81eccf82131ae4cfcc4cd31561d7
naitikiBio <naitikgcpcertifications@gmail.com>
2025-06-09 13:30:16 -0400

    Create etl_workflow_template.py

---
+++ etl_workflow_template.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import pendulum
+import logging
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.email import send_email
