c21c7020a0c2dd9cb40c7871156ffdba9dc402d5
naitikiBio <naitikgcpcertifications@gmail.com>
2025-06-10 11:06:37 -0400

    Create Website_data_extractor.py

---
+++ Website_data_extractor.py
@@ -0,0 +1,22 @@
+# This scrapes data from websites and stores in HTML
+# Storing in HTML helps as Python hits a website multiple times, essentially creating DoS attack, and also overloads the server
+# We are good people, we don't do that, and also, if the extraction is not correct, we always have the raw data to re-extract
+
+# --- Installation ---
+# Run this command in your terminal, I am assuming you already know about Python and virtual environments
+# pip install requests
+
+import requests
+import BeautifulSoup
+
+def save_html(html, path):
+  with open(path, 'wb') as f:
+    f.write(html)
+
+save_html(r.content, 'google_com')
+
+def open_html(path):
+  with open(path, 'rb') as f:
+    return f.read()
+
+html = open_html('google_com')
